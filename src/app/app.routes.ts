0d1632f70c78e2e90c0c836a3a966308dc6141a0
DulceST <ladroladebros864@gmail.com>
2024-05-13 21:23:15 -0600

    feat: Agregar rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ShopComponent } from './pages/shop/shop.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CartComponent } from './pages/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'shop', component: ShopComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'cart', component: CartComponent}
+];
